79062c07950be0a9308c15b6389f333ca7b529b6
Hansen Makangiras <hansenmakangiras@gmail.com>
2014-12-03 02:31:11 -0800

    Added Changes

---
+++ app/config/site.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HansenKibow
+ * Date: 02/12/2014
+ * Time: 9:21
+ */
+
+return array(
+    /**
+     *  Site Configuration
+     */
+
+    // Nama Site
+    'name' => 'PMK LP3I Makassar',
+);
